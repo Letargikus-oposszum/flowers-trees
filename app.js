2707e40a4af2ba28c5d0b77f58565e0e778c1711
Letargikus-oposszum <144096062+Letargikus-oposszum@users.noreply.github.com>
2025-02-17 13:04:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -40,25 +40,46 @@ app.get('/weboldal', (req, res) => {
     res.sendFile("./views/index.html", {root:__dirname});
 })
 
+const flowers = [
+    {name:"Pipacs", category:"evelo"},
+    {name:"Pitypang", category:"egyeves"},
+    {name:"Szazszorszep", category:"egyeves"}
+]
+
+const trees = [
+    {name:"tolgy", category:"lombhullato"},
+    {name:"Fenyo", category:"orokzold"},
+    {name:"Bukk", category:"lombhullato"}
+]
+
+app.get('/flowers', (req, res) => {
+    res.json(flowers);
+})
+app.get('/trees', (req, res) => {
+    res.json(trees);
+})
+app.get('/',(req, res) => {
+    res.sendFile("./views/error.html", {root:__dirname});
+})
 app.get('/:parameter', (req, res) => {
     res.send(req.params.parameter);
 })
 
-app.post('/flowers', (req, res) =>{
-    const name = req.body.name;
-    const category = req.body.category;
-    console.log(name, category);
-    const obj = {name, category}
-    //res.status(201).json({message: obj});
-    res.json(obj);
-})
+// app.post('/flowers', (req, res) =>{
+//     const name = req.body.name;
+//     const category = req.body.category;
+//     console.log(name, category);
+//     const obj = {name, category}
+//     //res.status(201).json({message: obj});
+//     res.json(obj);
+// })
 
-app.post('/trees', (req, res) =>{
-    const name = req.body.name;
-    const category = req.body.category;
-    console.log(name, category);
-    const obj = {name, category}
-    //res.status(201).json({message: obj});
-    res.json(obj);
-})
-app.listen(3000, () => {console.log("Server runs on port 3001");})
+// app.post('/trees', (req, res) =>{
+//     const name = req.body.name;
+//     const category = req.body.category;
+//     console.log(name, category);
+//     const obj = {name, category}
+//     //res.status(201).json({message: obj});
+//     res.json(obj);
+// })
+app.listen(3010, () => {console.log("Server runs on port 3010");})
